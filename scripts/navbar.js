df99499936cb3558491585770c44810fc5f20b5c
LucaPinheiro <lucapgomes11@gmail.com>
2023-09-15 11:32:46 -0300

    changing names and creating profile navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,16 @@
-menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("show");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.getElementById("menuBtn");
+  const menuBtn2 = document.getElementById("menuBtn2");
+  const nav = document.getElementById("nav");
+  const nav2 = document.getElementById("nav2");
+
+  menuBtn.addEventListener("click", () => {
+      nav.classList.toggle("show");
+      nav2.classList.remove("show"); 
   });
-  
+
+  menuBtn2.addEventListener("click", () => {
+      nav2.classList.toggle("show");
+      nav.classList.remove("show"); 
+  });
+});
