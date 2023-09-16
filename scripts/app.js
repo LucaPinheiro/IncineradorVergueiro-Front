982b8f939a4b27ef3ab5e6cb5062d359ec60e1b9
LucaPinheiro <lucapgomes11@gmail.com>
2023-09-16 03:59:39 -0300

    Starting carousel and adding events

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+import bannerSlide from "./banner/bannerSlide.js";
+
+const menuButton = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
+
+menuButton.addEventListener("click", () => {
+  nav.classList.toggle("nav--visible");
+});
+
+bannerSlide.updateCarousel();
---
+++ scripts/banner/bannerSlide.js
@@ -0,0 +1,38 @@
+const carouselData = [
+  {
+    title: "Eventos",
+    text: "INCINERADOR VERGUEIRO ESTÁ DE PORTAS ABERTAS! NOS PRÓXIMOS SÁBADOS DO MÊS DE SETEMBRO ESTARÁ ABERTA AS INSCRIÇÕES PARA O EVENTO.",
+    buttonText: "Inscreva-se",
+    buttonLink: "#",
+  },
+  {
+    title: "Abaixo Assinado",
+    text: "Contribua",
+    buttonText: "Acesse",
+    buttonLink: "#",
+  },
+  {
+    title: "Evento 3",
+    text: "Descrição do Evento 3.",
+    buttonText: "Inscreva-se 3",
+    buttonLink: "#",
+  },
+];
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const titleElement = document.getElementById("carouselTitle");
+  const textElement = document.getElementById("carouselText");
+  const currentItem = carouselData[currentIndex];
+  titleElement.textContent = currentItem.title;
+  textElement.textContent = currentItem.text;
+  currentIndex = (currentIndex + 1) % carouselData.length;
+}
+
+const interval = 4000;
+setInterval(updateCarousel, interval);
+
+updateCarousel();
+
+export default { updateCarousel };
