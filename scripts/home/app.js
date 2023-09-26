b54e63bec3b6cf02d9117c7322d996c1051be6e3
LucaPinheiro <lucapgomes11@gmail.com>
2023-09-26 10:24:16 -0300

    adjusting imports

--- scripts/home/app.js
+++ scripts/home/app.js
@@ -1,7 +1,7 @@
 import { updateCarousel } from "./banner/bannerSlide.js";
 import { carouselData } from "./banner/data/carouselData.js";
 import { createIndicators } from "./banner/indicators.js";
-import { toggleNavVisibility } from "./menuToggle.js"; 
+import { toggleNavVisibility } from "./navbar/menuToggle.js"; 
 
 const menuButton = document.getElementById("menuBtn");
 const nav = document.getElementById("nav");
