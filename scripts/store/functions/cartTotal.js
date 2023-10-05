9e9ca73210855f5a9179a27ac2c33fdfd95079cf
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-05 17:20:50 -0300

    adjusting function cartTotal

--- scripts/store/functions/cartTotal.js
+++ scripts/store/functions/cartTotal.js
@@ -1,21 +1,22 @@
 export function updateTotal() {
-  const cartTableBody = document.querySelector("#cartTable tbody");
-  const cartItems = Array.from(cartTableBody.querySelectorAll("tr"));
-
-  let total = 0;
-
-  cartItems.forEach((item) => {
-    const productPriceCell = item.querySelector("td:nth-child(3)");
-    const quantityValue = item.querySelector(".quantity-value");
-
-    const price = parseFloat(
-      productPriceCell.textContent.replace("R$", "").replace(",", ".")
-    );
-    const quantity = parseInt(quantityValue.textContent);
-
-    total += price * quantity;
-  });
-
-  const cartTotalElement = document.getElementById("cartTotal");
-  cartTotalElement.textContent = `Total: R$ ${total.toFixed(2)}`;
-}
+    const cartTableBody = document.querySelector("#cartTable tbody");
+    const cartItems = Array.from(cartTableBody.querySelectorAll("tr"));
+  
+    let total = 0;
+  
+    cartItems.forEach((item) => {
+      const productPriceCell = item.querySelector(".product-price");
+      const quantityValue = item.querySelector(".quantity-value");
+  
+      const price = parseFloat(
+        productPriceCell.textContent.replace("R$", "").replace(",", ".")
+      );
+      const quantity = parseInt(quantityValue.textContent);
+  
+      total += price;
+    });
+  
+    const cartTotalElement = document.getElementById("cartTotal");
+    cartTotalElement.textContent = `Total: R$ ${total.toFixed(2)}`;
+  }
+  
