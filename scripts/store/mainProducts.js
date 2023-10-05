5329fbdacf1dc34b98509958d271275ecfc9fbc8
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-05 02:19:06 -0300

    adjusting code

--- scripts/store/mainProducts.js
+++ scripts/store/mainProducts.js
@@ -1,7 +1,13 @@
 import { generateProductHTML} from "./functions/productUtil.js";
 import { addProductToSection } from "./functions/addProductToSection.js";
 import { products } from "./productsData.js/products.js";
+import { clearCart } from "./functions/clearCart.js";
 
 for (const product of products) {
   addProductToSection(product.name, product.imageSrc, product.price, product.id, "productSection");
 }
+
+const clearCartIcon = document.getElementById("clearCartIcon");
+clearCartIcon.addEventListener("click", () => {
+  clearCart(); // Chame a função para limpar o carrinho
+});
