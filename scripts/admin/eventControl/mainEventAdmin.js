32919e3376031b7f61ca0428761c9fad47eb5422
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-21 17:40:30 -0300

    adding requests to admin event page

--- scripts/admin/eventControl/mainEventAdmin.js
+++ scripts/admin/eventControl/mainEventAdmin.js
@@ -1,50 +1,59 @@
-import eventsData from '../eventControl/data/eventsControlData.js';
-import { deleteEvent } from './functions/eventsUtil.js';
-import { openConfirmationModal } from './modalRemove/modalUtils.js';
+import { openConfirmationModal } from "./modalRemove/modalUtils.js";
 
-const eventCardsContainer = document.getElementById('eventCards');
+const eventCardsContainer = document.getElementById("eventCards");
 
-eventsData.forEach(event => {
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.id = `event-${event.id}`;
+fetch("http://localhost:3000/events")
+  .then((response) => response.json())
+  .then((eventsData) => {
+    eventsData.forEach((event) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.id = `event-${event._id}`;
 
-    const cardBox = document.createElement('div');
-    cardBox.className = 'card_box';
+      const cardBox = document.createElement("div");
+      cardBox.className = "card_box";
 
-    const namePara = document.createElement('p');
-    namePara.className = 'text';
-    namePara.textContent = `Nome do Evento: ${event.nome}`;
-    cardBox.appendChild(namePara);
+      const namePara = document.createElement("p");
+      namePara.className = "text";
+      namePara.textContent = `Nome do Evento: ${event.nameEvent}`;
+      cardBox.appendChild(namePara);
 
-    const descPara = document.createElement('p');
-    descPara.className = 'text';
-    descPara.textContent = `Descrição: ${event.descricao}`;
-    cardBox.appendChild(descPara);
+      const eventDate = new Date(event.date);
+      const formattedDate = `${eventDate.getDate()}/${
+        eventDate.getMonth() + 1
+      }`;
+      const datePara = document.createElement("p");
+      datePara.className = "text";
+      datePara.textContent = `Data: ${formattedDate}`;
+      cardBox.appendChild(datePara);
 
-    const idPara = document.createElement('p');
-    idPara.className = 'text';
-    idPara.textContent = `ID Evento: ${event.id}`;
-    cardBox.appendChild(idPara);
+      const descPara = document.createElement("p");
+      descPara.className = "text";
+      descPara.textContent = `Descrição: ${event.description}`;
+      cardBox.appendChild(descPara);
 
-    const buttons = document.createElement('div');
-    buttons.className = 'buttons';
+      const idPara = document.createElement("p");
+      idPara.className = "text";
+      idPara.textContent = `ID Evento: ${event._id}`;
+      cardBox.appendChild(idPara);
 
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'button';
-    deleteButton.textContent = 'Apagar';
-    deleteButton.addEventListener('click', () => {
-        openConfirmationModal(eventsData, event.id);
+      const buttons = document.createElement("div");
+      buttons.className = "buttons";
+
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "button";
+      deleteButton.textContent = "Apagar";
+      deleteButton.addEventListener("click", () => {
+        openConfirmationModal(event._id);
+      });
+      buttons.appendChild(deleteButton);
+
+      const editButton = document.createElement("button");
+      editButton.className = "button";
+
+      card.appendChild(cardBox);
+      card.appendChild(buttons);
+
+      eventCardsContainer.appendChild(card);
     });
-    buttons.appendChild(deleteButton);
-
-    const editButton = document.createElement('button');
-    editButton.className = 'button';
-    editButton.textContent = 'Editar';
-    buttons.appendChild(editButton);
-
-    card.appendChild(cardBox);
-    card.appendChild(buttons);
-
-    eventCardsContainer.appendChild(card);
-});
+  });
--- scripts/admin/eventControl/modalRemove/modalUtils.js
+++ scripts/admin/eventControl/modalRemove/modalUtils.js
@@ -1,23 +1,38 @@
-import { deleteEvent } from '../functions/eventsUtil.js';
+import { removeEventWithAnimation } from '../modalRemove/deleteEvent.js';
 
-export function openConfirmationModal(eventsData, eventId) {
-    const confirmationModal = document.getElementById('confirmationModal');
-    confirmationModal.style.display = 'block';
-
-    const confirmDeleteButton = document.getElementById('confirmDeleteButton');
-    const cancelDeleteButton = document.getElementById('cancelDeleteButton');
-
-    confirmDeleteButton.addEventListener('click', () => {
-        closeConfirmationModal();
-        deleteEvent(eventsData, eventId);
-    });
-
-    cancelDeleteButton.addEventListener('click', () => {
-        closeConfirmationModal();
-    });
+export function openConfirmationModal(eventId) {
+  const confirmationModal = document.getElementById("confirmationModal");
+  const confirmDeleteButton = document.getElementById("confirmDeleteButton");
+  const cancelDeleteButton = document.getElementById("cancelDeleteButton");
+  
+  const eventCard = document.getElementById(`event-${eventId}`);
+  
+  confirmDeleteButton.addEventListener("click", () => {
+    fetch(`http://localhost:3000/events/${eventId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          removeEventWithAnimation(eventCard);
+        } else {
+          console.error("Falha ao excluir o evento");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  
+    closeConfirmationModal();
+  });
+  
+  cancelDeleteButton.addEventListener("click", () => {
+    closeConfirmationModal();
+  });
+  
+  confirmationModal.style.display = "block";
 }
-
+  
 export function closeConfirmationModal() {
-    const confirmationModal = document.getElementById('confirmationModal');
-    confirmationModal.style.display = 'none';
+  const confirmationModal = document.getElementById("confirmationModal");
+  confirmationModal.style.display = "none";
 }
