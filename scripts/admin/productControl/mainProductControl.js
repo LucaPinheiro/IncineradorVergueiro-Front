36865fcb4e83ba80c7a98f3a35af2aa198c31e8c
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-23 09:45:15 -0300

    adjusting modal

--- scripts/admin/productControl/mainProductControl.js
+++ scripts/admin/productControl/mainProductControl.js
@@ -1,62 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addProductForm = document.getElementById("addProductForm");
-  const openConfirmationModalButton = document.getElementById("openConfirmationModal");
+  const openConfirmationModalButton = document.getElementById(
+    "openConfirmationModal"
+  );
   const confirmationModal = document.getElementById("confirmationModal");
+  const successModal = document.getElementById("successModal");
+  const closeSuccessButton = document.getElementById("closeSuccessButton");
   const confirmButton = document.getElementById("confirmButton");
   const cancelButton = document.getElementById("cancelButton");
+  const productNameInput = document.getElementById("productName");
+  const productValueInput = document.getElementById("productValue");
+  const productImageInput = document.getElementById("productImage");
 
   addProductForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      confirmationModal.style.display = "block";
+    e.preventDefault();
+    confirmationModal.style.display = "block";
   });
 
   confirmButton.addEventListener("click", async () => {
-      confirmationModal.style.display = "none";
-      const productNameInput = document.getElementById("productName");
-      const productValueInput = document.getElementById("productValue");
-      const productImageInput = document.getElementById("productImage");
+    confirmationModal.style.display = "none";
+    const nameProduct = productNameInput.value;
+    const price = parseFloat(productValueInput.value);
+    const urlImageProduct = productImageInput.value;
 
-      const nameProduct = productNameInput.value;
-      const price = parseFloat(productValueInput.value);
-      const urlImageProduct = productImageInput.value;
+    if (!nameProduct || isNaN(price) || !urlImageProduct) {
+      console.error("Por favor, preencha todos os campos corretamente.");
+      return;
+    }
 
-      if (!nameProduct || isNaN(price) || !urlImageProduct) {
-          console.error("Por favor, preencha todos os campos corretamente.");
-          return;
+    const productData = {
+      nameProduct,
+      urlImageProduct,
+      price,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(productData),
+      });
+
+      if (response.ok) {
+        successModal.style.display = "block";
+        productNameInput.value = "";
+        productValueInput.value = "";
+        productImageInput.value = "";
+      } else {
+        console.error("Ocorreu um erro ao adicionar o produto.");
       }
+    } catch (error) {
+      console.error("Erro durante a solicitação:", error);
+      console.error("Ocorreu um erro ao adicionar o produto.");
+    }
+  });
 
-      const productData = {
-          nameProduct,
-          urlImageProduct,
-          price,
-      };
-
-      console.log("Enviando dados do produto para o servidor:", productData);
-
-      try {
-          const response = await fetch("http://localhost:3000/products", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-              },
-              body: JSON.stringify(productData),
-          });
-
-          if (response.ok) {
-              console.log("Produto adicionado com sucesso.");
-              productNameInput.value = "";
-              productValueInput.value = "";
-              productImageInput.value = "";
-          } else {
-              console.error("Ocorreu um erro ao adicionar o produto.");
-          }
-      } catch (error) {
-          console.error("Erro durante a solicitação:", error);
-          console.error("Ocorreu um erro ao adicionar o produto.");
-      }
+  closeSuccessButton.addEventListener("click", () => {
+    successModal.style.display = "none";
   });
 
   cancelButton.addEventListener("click", () => {
-      confirmationModal.style.display = "none";
+    confirmationModal.style.display = "none";
   });
 });
