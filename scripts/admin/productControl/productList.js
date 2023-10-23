48f00a96636e32c16987a35d7e68e37155685246
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-23 10:47:54 -0300

    adding a get request to load products data

---
+++ scripts/admin/productControl/productList.js
@@ -0,0 +1,76 @@
+// productList.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const productsContainer = document.getElementById("products");
+  
+    // Função para carregar e listar os produtos
+    const loadProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/products");
+        if (!response.ok) {
+          throw new Error("Erro ao buscar produtos.");
+        }
+  
+        const products = await response.json();
+  
+        productsContainer.innerHTML = ""; // Limpa o conteúdo anterior
+  
+        products.forEach((product) => {
+          const productElement = document.createElement("div");
+          productElement.classList.add("product-item");
+          productElement.setAttribute("data-id", product._id);
+  
+          const productName = document.createElement("span");
+          productName.textContent = product.nameProduct;
+  
+          const productPrice = document.createElement("span");
+          productPrice.textContent = `R$ ${product.price.toFixed(2)}`;
+  
+          const deleteButton = document.createElement("img");
+          deleteButton.src = "../../assets/store/trash.svg";
+          deleteButton.classList.add("delete-button");
+          deleteButton.addEventListener("click", () => deleteProduct(product._id));
+  
+          productElement.appendChild(productName);
+          productElement.appendChild(productPrice);
+          productElement.appendChild(deleteButton);
+  
+          productsContainer.appendChild(productElement);
+        });
+      } catch (error) {
+        console.error("Erro ao carregar produtos:", error);
+      }
+    };
+  
+    // Função para excluir um produto
+    const deleteProduct = async (productId) => {
+      if (confirm("Tem certeza de que deseja excluir este produto?")) {
+        try {
+          const response = await fetch(`http://localhost:3000/products/${productId}`, {
+            method: "DELETE",
+          });
+  
+          if (response.ok) {
+            console.log("Produto excluído com sucesso!");
+            // Remover o produto da lista após a exclusão
+            const productToDelete = productsContainer.querySelector(`[data-id="${productId}"]`);
+            if (productToDelete) {
+              productToDelete.style.transition = "opacity 0.5s"; // Adicione a transição
+              productToDelete.style.opacity = 0;
+              setTimeout(() => {
+                productToDelete.remove();
+              }, 500);
+            }
+          } else {
+            console.error("Erro ao excluir o produto.");
+          }
+        } catch (error) {
+          console.error("Erro durante a solicitação:", error);
+          console.error("Ocorreu um erro ao excluir o produto.");
+        }
+      }
+    };
+  
+    loadProducts(); // Carregar os produtos quando a página carregar
+  });
+  
