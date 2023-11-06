8b7a606188ea8a5cd60118a910131d421e6c9e9f
LucaPinheiro <lucapgomes11@gmail.com>
2023-11-06 11:14:13 -0300

    adding function to delete user

--- scripts/admin/userControl/mainUser.js
+++ scripts/admin/userControl/mainUser.js
@@ -1,11 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const userTable = document.getElementById("userTable");
+  const table = document.querySelector(".table");
+
+  function createConfirmationModal(userId) {
+    const modal = document.getElementById("deleteUserModal");
+    modal.style.display = "block";
+
+    const confirmButton = document.getElementById("confirmDelete");
+    const cancelButton = document.getElementById("cancelDelete");
+
+    confirmButton.addEventListener("click", () => {
+      try {
+        fetch(`http://localhost:3000/users/${userId}`, {
+          method: "DELETE",
+        })
+          .then((response) => response.json())
+          .then(() => {
+            const rowToDelete = document.querySelector(
+              `[data-user-id="${userId}"]`
+            );
+            if (rowToDelete) {
+              table.deleteRow(rowToDelete.rowIndex);
+            }
+
+            modal.style.display = "none";
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error("Erro ao excluir o usuário:", error);
+            modal.style.display = "none";
+          });
+      } catch (error) {
+        console.error("Erro ao excluir o usuário:", error);
+        modal.style.display = "none";
+      }
+    });
+
+    cancelButton.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  }
 
   fetch("http://localhost:3000/users/")
     .then((response) => response.json())
     .then((data) => {
       data.forEach((user) => {
-        const row = userTable.insertRow();
+        const row = table.insertRow();
+        row.setAttribute("data-user-id", user._id);
 
         const nameCell = row.insertCell(0);
         nameCell.innerHTML = `<p>${user.name}</p>`;
@@ -15,6 +55,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const cpfCell = row.insertCell(2);
         cpfCell.textContent = user.cpf;
+
+        const deleteCell = row.insertCell(3);
+        const deleteIcon = document.createElement("img");
+        deleteIcon.src = "../../assets/store/trash.svg";
+        deleteIcon.alt = "Lixeira";
+        deleteCell.appendChild(deleteIcon);
+
+        deleteIcon.addEventListener("click", () => {
+          createConfirmationModal(user._id);
+        });
       });
     })
     .catch((error) => {
